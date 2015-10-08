b4cc4921350b014a730fe59148eaac5318f40190
Start Coding Student 2 <session2@START-Coding-MBP-C02PP8Q8G8WN.local>
2015-10-07 18:09:01 -0700

    Added the JavaScript file that I edited the on JSFIddle, and transferred it over to Phpstorm.

--- js..js
+++ js..js
@@ -1,3 +1,30 @@
-/**
- * Created by session2 on 10/7/15.
- */
+google.load('visualization', '1', {packages: ['corechart', 'bar']});
+google.setOnLoadCallback(drawBasic);
+
+function drawBasic() {
+
+    var data = google.visualization.arrayToDataTable([
+        ['Year', 'Visitors',],
+        ['2008-2009', 1556162],
+        ['2009-2010', 1459080],
+        ['2010-2011', 1543232],
+        ['2011-2012', 1660450],
+        ['2012-2013', 1506274]
+    ]);
+
+    var options = {
+        title: 'L.A. Zoo Attendence 2008-2013.',
+        chartArea: {width: '50%'},
+        hAxis: {
+            title: 'Visitors.',
+            minValue: 0
+        },
+        vAxis: {
+            title: 'Year'
+        }
+    };
+
+    var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+
+    chart.draw(data, options);
+}
